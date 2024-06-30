0b2f92e5fda881baddac3b3dff0ac0be82045360
Brian <100100729+Braiane@users.noreply.github.com>
2024-06-30 12:40:20 +0200

    Add files via upload

---
+++ Calculatrice/app.js
@@ -0,0 +1,41 @@
+const premier = document.querySelector(".first");
+const deuxieme = document.querySelector(".second");
+const calculer = document.querySelector(".resultat");
+const operationSelect = document.getElementById("operation");
+
+// Écouteur d'événement sur le bouton résultat
+calculer.addEventListener("click", calcule);
+
+function calcule(e) {
+  e.preventDefault();
+
+  // Récupération des valeurs des champs input
+  var un = parseFloat(premier.value);
+  var deux = parseFloat(deuxieme.value);
+  var result;
+
+  // Récupération de l'opération sélectionnée dans le select
+  var operation = operationSelect.value;
+
+  // Switch sur l'opération sélectionnée
+  switch (operation) {
+    case "plus":
+      result = un + deux;
+      break;
+    case "moins":
+      result = un - deux;
+      break;
+    case "fois":
+      result = un * deux;
+      break;
+    case "diviser":
+      result = un / deux;
+      break;
+    default:
+      result = "Opération invalide";
+  }
+
+  // Affichage du résultat dans l'élément resultat
+  console.log(result); // Vérifiez le résultat dans la console
+  document.getElementById("resultats").innerText = result;
+}
